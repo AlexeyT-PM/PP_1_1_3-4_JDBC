0abd84e6ce65b0946088eecb915aa0871ec9b4cd
AlexeyT-PM <alexey.tugov@gmail.com>
2023-10-25 17:04:33 +0300

    commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,85 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+
 public class UserDaoJDBCImpl implements UserDao {
+    private static final Connection connection = Util.getConnection();
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
+        String SQLcreat = "CREATE TABLE IF NOT EXISTS kataTest (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(40), last_name VARCHAR(40), age INT)";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(SQLcreat);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        String SQLdrop = "DROP TABLE IF EXISTS kataTest";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(SQLdrop);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String SQLsave = "INSERT INTO kataTest (name, last_name, age) VALUES (?, ?, ?)";
+        try (PreparedStatement pstm = connection.prepareStatement(SQLsave)) {
+            pstm.setString(1, name);
+            pstm.setString(2, lastName);
+            pstm.setByte(3, age);
+            pstm.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+//        String SQLremove = "SELECT * FROM kataTest " + id;
+        String SQLremove = "DELETE FROM kataTest WHERE id= " + id;
+        try (PreparedStatement pstm = connection.prepareStatement(SQLremove)) {
+            pstm.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> usersList = new ArrayList<>();
+        String SQLget = "SELECT * FROM kataTest";
+        try (ResultSet resultSet = connection.createStatement().executeQuery(SQLget)) {
+            while (resultSet.next()) {
+                User user = new User(resultSet.getString("name"),
+                        resultSet.getString("last_name"), resultSet.getByte("age"));
+                user.setId(resultSet.getLong("id"));
+                usersList.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return usersList;
     }
 
     public void cleanUsersTable() {
-
+        String SQLclean = "TRUNCATE TABLE kataTest";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(SQLclean);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,44 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final static String URL =
+            "jdbc:mysql://localhost:3306/new_schema";
+//    &useLegacyDatetimeCode=false&serverTimezone=UTC
+//
+    /*    private final static String URLFIXED =
+                "jdbc:mysql://localhost:3306/new_schema?useUnicode=true&useSSL=true&useJDBCCompliantTimezoneShift=true" +
+                        "&useLegacyDatetimeCode=false&serverTimezone=UTC";*/
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        Connection connection;
+            try {
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                if (!connection.isClosed()) {
+                    System.out.println("Соединение с БД Установлено!");
+                }
+                if (connection.isClosed()) {
+                    System.out.println("Соединение с БД Закрыто!");
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        return connection;
+    }
+// реализуйте настройку соеденения с БД
 }
